2634df3f1228ecbf925d0658e9e3168ea13aa75c
julesparisis-ctrl <jules.parisis@icloud.com>
2026-06-30 10:19:03 -0300

    Create etape-1.js

---
+++ assets/data/etape-1.js
@@ -0,0 +1,30 @@
+// assets/data/etape-1.js
+
+export default {
+  id: "etape-1",
+  type: "americano",
+
+  titre: "Étape 1 - Americano",
+  nomCourt: "Americano",
+  date: "02 juillet 2026",
+  heure: "18h30",
+  lieu: "Tennis Club de Suzini",
+
+  public: "Ouvert à tous",
+  licence: "Licenciés et non licenciés",
+
+  inscription: {
+    ouverte: true,
+    tarif: "À préciser",
+    lien: "inscriptions.html"
+  },
+
+  navigation: {
+    accueil: "index.html",
+    inscriptions: "inscriptions.html",
+    participants: "participants.html",
+    programmation: "programmation.html",
+    classement: "classement.html",
+    statistiques: "statistiques.html"
+  }
+};
